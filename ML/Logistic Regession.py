904a152b424ea45c4e61d1d6b2879d8722621d1c
Shivam Pahariya <84902682+shivam6225@users.noreply.github.com>
2023-05-16 00:00:07 +0530

    Create Logistic Regession.py

---
+++ ML/Logistic Regession.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+import sklearn.metrics as sm
+
+X= np.array([3.78,2.44,1.71,1.02,0.89,3.44,4.55,3.90,5.88,4.80]).reshape(-1,1)
+Y=np.array([0,0,0,0,0,1,1,1,1,1])
+from sklearn.linear_model import LogisticRegression
+
+logr=LogisticRegression()
+logr.fit(X,Y)
+
+y_predict=logr.predict(X)
+y_predict
+
+sm.accuracy_score(Y,y_predict)
